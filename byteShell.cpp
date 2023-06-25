46b36de4e83a2b0e7a500128aab6c6440788b2a8
Bhoomi Bonal <b_bonal@cs.iitr.ac.in>
2023-06-25 22:43:10 +0530

    Handle background process

--- byteShell.cpp
+++ byteShell.cpp
@@ -11,53 +11,81 @@ using namespace std;
 vector<string> history;
 int history_index = 0;
 
-int changeDirectory(char **args);
-int showHistory(char **args);
-int exit(char **args);
+int changeDirectory(vector<string> args);
+int showHistory(vector<string> args);
+int exitProgram(vector<string> args);
+int background(vector<string> args);
 
-map<std::string, int (*)(char **)> builtins = {
+map<string, int (*)(vector<string>)> builtins = {
     {"cd", changeDirectory},
-    {"exit", exit},
-    {"history", showHistory}};
+    {"exit", exitProgram},
+    {"history", showHistory},
+    {"bg", background}};
 
-vector<char *> split(const string &str, char delimiter)
+vector<string> split(const string &str, char delimiter)
 {
     vector<string> tokens;
     string token;
     istringstream tokenStream(str);
     while (getline(tokenStream, token, delimiter))
         tokens.push_back(token);
-    vector<char *> args;
-    for (auto &token : tokens)
-        args.push_back(const_cast<char *>(token.c_str()));
-    args.push_back(nullptr);
-    return args;
+    return tokens;
 }
 
-int changeDirectory(char **args)
+int changeDirectory(vector<string> args)
 {
     cout << "Changing Directory\n";
-    if (args[1] == NULL)
+    if (args.size() < 2)
         cout << "Error: Expected argument to \"cd\"" << endl;
-    else if (chdir(args[1]) != 0)
+    else if (chdir(args[1].c_str()) != 0)
         perror("ByteShellError");
     return 1;
 }
 
-int showHistory(char **args)
+string command(vector<string> args)
+{
+    string command = "";
+    for (auto arg : args)
+    {
+        command += arg;
+        command += " ";
+    }
+    return command;
+}
+
+int showHistory(vector<string> args)
 {
     cout << "Showing history\n";
-    for (const string &command : history)
+    for (auto &command : history)
         cout << command << endl;
     return 1;
 }
 
-int launchExtProgram(char **args)
+int background(vector<string> args)
 {
+    cout << "Bg";
+    return 1;
+}
+
+void handleSignal(int signal)
+{
+    int status;
+    pid_t pid;
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0)
+        cout << "Background process with PID " << pid << " completed" << endl;
+}
+
+int launchExtProgram(vector<string> args, bool backgroundProcess)
+{
+    vector<char *> charArgs;
+    for (auto &arg : args)
+        charArgs.push_back(const_cast<char *>(arg.c_str()));
+    charArgs.push_back(nullptr);
+
     pid_t pid = fork();
     if (pid == 0)
     {
-        if (execvp(args[0], args) == -1)
+        if (execvp(charArgs[0], charArgs.data()) == -1)
             perror("ByteShellError");
         exit(EXIT_FAILURE);
     }
@@ -65,26 +93,41 @@ int launchExtProgram(char **args)
         perror("ByteShellError");
     else
     {
-        int status;
-        waitpid(pid, &status, 0);
+        if (!backgroundProcess)
+        {
+            int status;
+            waitpid(pid, &status, 0);
+        }
+        else
+        {
+            signal(SIGCHLD, handleSignal);
+            cout << "'" << command(args) << "' command initialized with PID: " << pid << endl;
+        }
         return 1;
     }
     return -1;
 }
 
-int execute(char **args)
+int execute(vector<string> args)
 {
-    if (args[0] == NULL)
+    if (args.empty())
         return 1;
 
     for (auto &builtin : builtins)
         if (args[0] == builtin.first)
             return builtin.second(args);
 
-    return launchExtProgram(args);
+    int argsCount = args.size();
+    bool backgroundProcess = (argsCount > 1 && args[argsCount - 1] == "&");
+    if (backgroundProcess)
+    {
+        args.pop_back();
+        cout << "Initializing background process\n";
+    }
+    return launchExtProgram(args, backgroundProcess);
 }
 
-int exit(char **args)
+int exitProgram(vector<string> args)
 {
     return -1;
 }
@@ -105,8 +148,8 @@ int main()
             history.push_back(line);
         history_index = history.size();
         add_history(line.c_str());
-        vector<char *> args = split(line, ' ');
-        status = execute(args.data());
+        vector<string> args = split(line, ' ');
+        status = execute(args);
     } while (status != -1);
 
     return 0;
